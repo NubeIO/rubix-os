dd4a97d1c4455a12bbebeb5c58c74d8bbbea3199
NubeDev <aidan@enviroservices.com.au>
2021-09-13 19:04:24 +1000

    added clien writer

---
+++ client/writer.go
@@ -0,0 +1,59 @@
+package client
+
+import (
+	"fmt"
+	"github.com/NubeDev/flow-framework/model"
+	"github.com/NubeDev/flow-framework/utils"
+	"strconv"
+)
+
+// GetWriter an object
+func (a *FlowClient) GetWriter(uuid string) (*model.Writer, error) {
+	resp, err := a.client.R().
+		SetResult(&model.Writer{}).
+		SetPathParams(map[string]string{"uuid": uuid}).
+		Get("/api/consumers/writers/{uuid}")
+	if err != nil {
+		return nil, fmt.Errorf("fetch name for name %s failed", err)
+	}
+	if resp.Error() != nil {
+		return nil, getAPIError(resp)
+	}
+	return resp.Result().(*model.Writer), nil
+}
+
+// EditWriter edit an object
+func (a *FlowClient) EditWriter(uuid string, body model.Writer, updateProducer bool) (*model.Writer, error) {
+	param := strconv.FormatBool(updateProducer)
+	resp, err := a.client.R().
+		SetResult(&model.Writer{}).
+		SetBody(body).
+		SetPathParams(map[string]string{"uuid": uuid}).
+		SetQueryParam("update_producer", param).
+		Patch("/api/consumers/writers/{uuid}")
+	if err != nil {
+		return nil, fmt.Errorf("fetch name for name %s failed", err)
+	}
+	if resp.Error() != nil {
+		return nil, getAPIError(resp)
+	}
+	return resp.Result().(*model.Writer), nil
+}
+
+
+// CreateWriter edit an object
+func (a *FlowClient) CreateWriter(body model.Writer) (*model.Writer, error) {
+	name, _ := utils.MakeUUID()
+	name = fmt.Sprintf("sub_name_%s", name)
+	resp, err := a.client.R().
+		SetResult(&model.Writer{}).
+		SetBody(body).
+		Post("/api/consumers/writers")
+	if err != nil {
+		return nil, fmt.Errorf("fetch name for name %s failed", err)
+	}
+	if resp.Error() != nil {
+		return nil, getAPIError(resp)
+	}
+	return resp.Result().(*model.Writer), nil
+}
