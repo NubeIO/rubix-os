42077dbcd932b793a838d76a56dd6cd0c619f2f5
NubeDev <aidan@enviroservices.com.au>
2021-09-28 08:56:44 +1000

    added shell for mqtt broker plugin

---
+++ plugin/nube/protocals/broker/enable.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"errors"
+	"github.com/NubeDev/flow-framework/api"
+)
+
+// Enable implements plugin.Plugin
+func (i *Instance) Enable() error {
+	i.enabled = true
+	i.setUUID()
+	i.BusServ()
+	var arg api.Args
+	q, err := i.db.GetNetworkByPlugin(i.pluginUUID, arg)
+	if err != nil {
+		return errors.New("there is no network added please add one")
+	}
+	i.networkUUID = q.UUID
+	if err != nil {
+		return errors.New("error on enable lora-plugin")
+
+	}
+	return nil
+}
+
+// Disable implements plugin.Disable
+func (i *Instance) Disable() error {
+	i.enabled = false
+	return nil
+}
