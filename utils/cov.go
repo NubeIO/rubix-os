0b013d4d7e4c68dcdcec843aba8b6df51bb379af
Marc Dallen <marc@enviroservices.com.au>
2021-09-28 08:15:48 +1000

    Updated and Tested cov.go and nums.go functions.

--- utils/cov.go
+++ utils/cov.go
@@ -1,12 +1,12 @@
 package utils
 
-import "math"
-
-
+/*
 func COV(new float64, existingData float64, threshold float64) (bool, float64) {
 	if math.Abs(new - existingData) >= threshold {
-		return true, existingData
+		return true, new
 	} else {
-		return false, new
+		return false, existingData
 	}
 }
+
+*/
--- utils/nums.go
+++ utils/nums.go
@@ -6,28 +6,72 @@ import (
 	"time"
 )
 
-func Round(n, unit float64) float64 {
-	if unit <= 0 {
-		return n
+//LimitToRange returns the input value clamped within the specified range
+func LimitToRange(value float64, range1 float64, range2 float64) float64 {
+	if range1 == range2 {
+		return range1
 	}
-	return math.Round(n/unit) * unit
+	var min, max float64
+	if range1 > range2 {
+		max = range1
+		min = range2
+	} else {
+		max = range2
+		min = range1
+	}
+	return math.Min(math.Max(value, min), max)
 }
 
-func RoundTo(n float64, decimals uint32) float64 {
-	if decimals <= 0 {
-		return n
+//Scale returns the (float64) input value (between inputMin and inputMax) scaled to a value between outputMin and outputMax
+func Scale(value float64, inMin float64, inMax float64, outMin float64, outMax float64) float64 {
+	scaled := ((value-inMin)/(inMax-inMin))*(outMax-outMin) + outMin
+	if scaled > math.Max(outMin, outMax) {
+		return math.Max(outMin, outMax)
+	} else if scaled < math.Min(outMin, outMax) {
+		return math.Min(outMin, outMax)
+	} else {
+		return scaled
 	}
-	return math.Round(n*math.Pow(10, float64(decimals))) / math.Pow(10, float64(decimals))
 }
 
-//RandInt with min and max range
-func RandInt(min, max int) int {
+//RoundTo returns the input value rounded to the specified number of decimal places.
+func RoundTo(value float64, decimals uint32) float64 {
+	if decimals < 0 {
+		return value
+	}
+	return math.Round(value*math.Pow(10, float64(decimals))) / math.Pow(10, float64(decimals))
+}
+
+//RandInt returns a random int within the specified range.
+func RandInt(range1, range2 int) int {
+	if range1 == range2 {
+		return range1
+	}
+	var min, max int
+	if range1 > range2 {
+		max = range1
+		min = range2
+	} else {
+		max = range2
+		min = range1
+	}
 	rand.Seed(time.Now().UnixNano())
 	return min + rand.Intn(max-min)
 }
 
-//RandFloat with min and max range
-func RandFloat(min, max float64) float64 {
+//RandFloat returns a random float64 within the specified range.
+func RandFloat(range1, range2 float64) float64 {
+	if range1 == range2 {
+		return range1
+	}
+	var min, max float64
+	if range1 > range2 {
+		max = range1
+		min = range2
+	} else {
+		max = range2
+		min = range1
+	}
 	rand.Seed(time.Now().UnixNano())
 	return min + rand.Float64()*(max-min)
 }
