54d1b2f5d09cb2ace9456b4b0e936af65fb6e875
NubeDev <aidan@enviroservices.com.au>
2021-09-28 10:26:03 +1000

    added clean up of mqtt broker plugin

--- plugin/nube/protocals/broker/api.go
+++ plugin/nube/protocals/broker/api.go
@@ -2,28 +2,15 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	log "github.com/sirupsen/logrus"
 	"net/http"
-	"time"
 )
 
 // RegisterWebhook implements plugin.Webhooker
 func (i *Instance) RegisterWebhook(basePath string, mux *gin.RouterGroup) {
 	i.basePath = basePath
 	//restart plugin
-	mux.POST("/broker/restart", func(ctx *gin.Context) {
-		err := i.Disable()
-		if err != nil {
-			log.Error("LORA: error on restart (disable) plugin %s", err)
-			ctx.JSON(http.StatusBadRequest, "restart fail")
-		}
-		time.Sleep(300 * time.Millisecond)
-		err = i.Enable()
-		if err != nil {
-			log.Error("LORA: error on restart (enable) plugin %s", err)
-			ctx.JSON(http.StatusBadRequest, "restart fail")
-		}
-		ctx.JSON(http.StatusOK, "restart ok")
+	mux.POST("/broker", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "ok")
 	})
 
 }
