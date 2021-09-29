4cb1e0b7718d37c415a67871ba59e2cfb816ea9f
RaiBnod <binodrayee@gmail.com>
2021-09-29 13:30:58 +0545

    Just an optimization on run build script

---
+++ build.bash
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# Console colors
+DEFAULT="\033[0m"
+GREEN="\033[32m"
+RED="\033[31m"
+
+PRODUCTION=false
+
+help() {
+    echo "Service commands:"
+    echo -e "   ${GREEN}--prod | --production: add these suffix to start production"
+}
+
+parseCommand() {
+    for i in "$@"; do
+        case ${i} in
+        -h | --help)
+            help
+            exit 0
+            ;;
+        --prod | --production)
+            PRODUCTION=true
+            ;;
+        *)
+            echo -e "${RED}Unknown options ${i}  (-h, --help for help)${DEFAULT}"
+            ;;
+        esac
+    done
+}
+
+parseCommand "$@"
+
+dir=$(pwd)
+echo -e "${GREEN}Current working directory is: $dir${DEFAULT}"
+pluginDir=$dir/data/plugins
+
+if [ ${PRODUCTION} == true ]; then
+    echo -e "${GREEN}We are running in production mode!${DEFAULT}"
+    pluginDir=/data/flow-framework/data/plugin
+else
+    echo -e "${GREEN}We are running in development mode!${DEFAULT}"
+fi
+
+echo -e "${GREEN}Creating a plugin directory if does not exist at: ${pluginDir}${DEFAULT}"
+rm -r $pluginDir/*
+mkdir -p $pluginDir
+cd $dir/plugin/nube/system/
+go build -buildmode=plugin -o system.so *.go  && cp system.so  $pluginDir
+
+cd $dir/plugin/nube/protocals/lorawan
+go build -buildmode=plugin -o lorawan.so *.go  && cp lorawan.so $pluginDir
+
+cd $dir/plugin/nube/protocals/modbus
+go build -buildmode=plugin -o modbus.so *.go  && cp modbus.so $pluginDir
+
+cd $dir/plugin/nube/protocals/lora
+go build -buildmode=plugin -o lora.so *.go  && cp lora.so $pluginDir
+
+cd $dir/plugin/nube/protocals/bacnetserver
+go build -buildmode=plugin -o bacnetserver.so *.go  && cp bacnetserver.so $pluginDir
+
+cd $dir/plugin/nube/database/influx
+go build -buildmode=plugin -o influx.so *.go  && cp influx.so $pluginDir
+
+cd $dir/plugin/nube/protocals/broker
+go build -buildmode=plugin -o broker.so *.go  && cp broker.so $pluginDir
+
+cd $dir
+go run app.go -g /data/flow-framework  -d data --prod
