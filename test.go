503ade4ab2aeb8b8af279906b6deaff9c9b7b9a8
NubeDev <aidan@enviroservices.com.au>
2021-10-19 17:16:33 +1100

    clean up of schedules

--- test.go
+++ test.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"github.com/NubeDev/flow-framework/src/schedule"
 	log "github.com/sirupsen/logrus"
 	"time"
 )
@@ -10,11 +8,7 @@ import (
 func main() {
 	//var wg sync.WaitGroup
 	log.Println("Starting Weekly Checks")
-	check, err := schedule.WeeklyCheck("./src/schedule/weekly_schedule.json", "TEST")
-	if err != nil {
-		return
-	}
-	fmt.Println(check.IsActive)
+
 }
 
 func forever() {
