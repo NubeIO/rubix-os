04048d062f0951862dda7d1b35459a3f5f83c067
NubeDev <aidan@enviroservices.com.au>
2021-10-19 15:44:35 +1100

    clean up of schedules

--- plugin/nube/system/schedule.go
+++ plugin/nube/system/schedule.go
@@ -1,36 +1,32 @@
 package main
 
 import (
-	"fmt"
 	"github.com/NubeDev/flow-framework/src/jobs"
 	"github.com/NubeDev/flow-framework/src/schedule"
+	log "github.com/sirupsen/logrus"
 )
 
-func (i *Instance) task() {
-	fmt.Println(1111111111)
+func (i *Instance) run() {
 	schedules, err := i.db.GetSchedules()
 	if err != nil {
-		return
+		log.Infof("system-plugin-schedule: db get scheudles %v\n", err)
 	}
-
 	for _, sch := range schedules {
-		check, err := schedule.WeeklyCheck(sch.Schedules, "Branch")
+		check, err := schedule.WeeklyCheck(sch.Schedules, sch.Name)
 		if err != nil {
 			return
 		}
-		fmt.Println(check.IsActive)
+		log.Infof("system-plugin-schedule: schedule is active %v\n", check.IsActive)
+		log.Infof("system-plugin-schedule: schedule payload %v\n", check.Payload)
 	}
-
 }
 
 func (i *Instance) schedule() {
-
 	j, ok := jobs.GetJobService()
 	if ok {
-		_, err := j.Every(10).Second().Tag("foo").Do(i.task)
+		_, err := j.Every(30).Second().Tag("foo").Do(i.run)
 		if err != nil {
-			//return
+			log.Infof("system-plugin-schedule: error on create job %v\n", err)
 		}
 	}
-
 }
