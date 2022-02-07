ecdb6fca72b54c28c4ddf4ade0446d7e7cb8dbd5
NubeDev <aidan@enviroservices.com.au>
2022-02-07 17:14:02 +1100

    added bacnetmaster plugin

---
+++ plugin/nube/protocals/bacnetmaster/bus.go
@@ -0,0 +1,181 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/NubeIO/flow-framework/eventbus"
+	"github.com/NubeIO/flow-framework/utils"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/mustafaturan/bus/v3"
+	log "github.com/sirupsen/logrus"
+)
+
+func (i *Instance) BusServ() {
+	handlerCreated := bus.Handler{ //CREATED
+		Handle: func(ctx context.Context, e bus.Event) {
+			go func() {
+				//try and match is network
+				net, err := eventbus.IsNetwork(e.Topic, e)
+				if err != nil {
+					return
+				}
+				if net != nil {
+					log.Info("BACNET BUS PluginsCreated isNetwork", " ", net.UUID)
+					if err != nil {
+						return
+					}
+					return
+				}
+				//try and match is device
+				dev, err := eventbus.IsDevice(e.Topic, e)
+				if err != nil {
+					return
+				}
+				if dev != nil {
+					log.Info("BACNET BUS PluginsCreated IsDevice", " ", dev.UUID)
+					//_, err = i.addPoints(dev)
+					if err != nil {
+						return
+					}
+					return
+				}
+				//try and match is point
+				pnt, err := eventbus.IsPoint(e.Topic, e)
+				fmt.Println("BACNET ADD POINT ON BUS")
+				if err != nil {
+					return
+				}
+				_, err = i.addPoint(pnt)
+				if err != nil {
+					log.Error("BACNET ADD POINT BUS issue on add rest")
+					return
+				}
+				if pnt != nil {
+					log.Info("BACNET BUS PluginsCreated IsPoint", " ", pnt.UUID)
+					if err != nil {
+						return
+					}
+					return
+				}
+			}()
+		},
+		Matcher: eventbus.PluginsCreated,
+	}
+	u, _ := utils.MakeUUID()
+	key := fmt.Sprintf("key_%s", u)
+	eventbus.GetBus().RegisterHandler(key, handlerCreated)
+	handlerUpdated := bus.Handler{ //UPDATED
+		Handle: func(ctx context.Context, e bus.Event) {
+			go func() {
+				//try and match is network
+				net, err := eventbus.IsNetwork(e.Topic, e)
+				if err != nil {
+					return
+				}
+				if net != nil {
+					log.Info("BACNET BUS PluginsUpdated isNetwork", " ", net.UUID)
+					if err != nil {
+						return
+					}
+					return
+				}
+				//try and match is device
+				dev, err := eventbus.IsDevice(e.Topic, e)
+				if err != nil {
+					return
+				}
+				if dev != nil {
+					//_, err = i.addPoints(dev)
+					log.Info("BACNET BUS PluginsUpdated IsDevice", " ", dev.UUID)
+					if err != nil {
+						return
+					}
+					return
+				}
+				//try and match is point
+				pnt, err := eventbus.IsPoint(e.Topic, e)
+				if err != nil {
+					return
+				}
+				if pnt != nil {
+					_, err = i.pointPatch(pnt)
+					log.Info("BACNET BUS PluginsUpdated IsPoint", " ", pnt.UUID)
+					if err != nil {
+						return
+					}
+					return
+				}
+			}()
+		},
+		Matcher: eventbus.PluginsUpdated,
+	}
+	u, _ = utils.MakeUUID()
+	key = fmt.Sprintf("key_%s", u)
+	eventbus.GetBus().RegisterHandler(key, handlerUpdated)
+	handlerDeleted := bus.Handler{ //DELETED
+		Handle: func(ctx context.Context, e bus.Event) {
+			go func() {
+				log.Info("BACNET BUS DELETED NEW MSG", " ", e.Topic)
+				//try and match is network
+				net, err := eventbus.IsNetwork(e.Topic, e)
+				if err != nil {
+					return
+				}
+				if net != nil {
+					log.Info("BACNET BUS DELETED isNetwork", " ", net.UUID)
+					if err != nil {
+						return
+					}
+					return
+				}
+				//try and match is device
+				dev, err := eventbus.IsDevice(e.Topic, e)
+				if err != nil {
+					return
+				}
+				if dev != nil {
+					//_, err = i.addPoints(dev)
+					log.Info("BACNET BUS DELETED IsDevice", " ", dev.UUID)
+					if err != nil {
+						return
+					}
+					return
+				}
+				//try and match is point
+				pnt, err := eventbus.IsPoint(e.Topic, e)
+				if err != nil {
+					return
+				}
+				log.Info("BACNET BUS DELETED IsPoint", " ")
+				if pnt != nil {
+					p, err := i.deletePoint(pnt)
+					log.Info("BACNET BUS DELETED IsPoint", " ", pnt.UUID, "WAS DELETED", " ", p)
+					if err != nil {
+						return
+					}
+					return
+				}
+			}()
+		},
+		Matcher: eventbus.PluginsDeleted,
+	}
+	u, _ = utils.MakeUUID()
+	key = fmt.Sprintf("key_%s", u)
+	eventbus.GetBus().RegisterHandler(key, handlerDeleted)
+	handlerMQTT := bus.Handler{ //MQTT UPDATE (got as msg over from bacnet stack)
+		Handle: func(ctx context.Context, e bus.Event) {
+			go func() {
+				p, _ := e.Data.(mqtt.Message)
+				_, err := i.bacnetUpdate(p)
+				if err != nil {
+					return
+				}
+			}()
+		},
+		Matcher: eventbus.MQTTUpdated,
+	}
+	u, _ = utils.MakeUUID()
+	key = fmt.Sprintf("key_%s", u)
+	eventbus.GetBus().RegisterHandler(key, handlerMQTT)
+
+}
---
+++ plugin/nube/protocals/bacnetmaster/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"github.com/NubeIO/flow-framework/eventbus"
+	"github.com/NubeIO/flow-framework/plugin/plugin-api"
+	"github.com/NubeIO/flow-framework/src/cachestore"
+	"github.com/NubeIO/flow-framework/src/dbhandler"
+	"github.com/NubeIO/nubeio-rubix-lib-helpers-go/pkg/nrest"
+)
+
+const path = "bacnetmaster" //must be unique across all plugins
+const name = "bacnetmaster" //must be unique across all plugins
+const description = "bacnet master api to nube bacnet stack"
+const author = "ap"
+const webSite = "https://www.github.com/NubeIO"
+const protocolType = "ip"
+
+const pluginType = "protocol"
+const allowConfigWrite = false
+const isNetwork = true
+const maxAllowedNetworks = 1
+const networkType = "bacnet"
+const transportType = "ip" //serial, ip
+const ip = "0.0.0.0"
+const port = 1717
+
+var rt = &nrest.ReqType{
+	BaseUri: ip,
+	Port:    port,
+	Debug:   true,
+}
+
+// Instance is plugin instance
+type Instance struct {
+	config      *Config
+	enabled     bool
+	basePath    string
+	db          dbhandler.Handler
+	store       cachestore.Handler
+	bus         eventbus.BusService
+	pluginUUID  string
+	networkUUID string
+}
+
+// GetFlowPluginInfo returns plugin info.
+func GetFlowPluginInfo() plugin.Info {
+	return plugin.Info{
+		ModulePath:   path,
+		Name:         name,
+		Description:  description,
+		Author:       author,
+		Website:      webSite,
+		HasNetwork:   true,
+		ProtocolType: protocolType,
+	}
+}
+
+// NewFlowPluginInstance creates a plugin instance for a user context.
+func NewFlowPluginInstance() plugin.Plugin {
+	return &Instance{}
+}
+
+//main will not let main run
+func main() {
+	panic("this should be built as plugin")
+}
