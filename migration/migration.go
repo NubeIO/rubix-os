6634d7c59a49dc4c32f2923c7e92f66a96a9a54a
RaiBnod <binodrayee@gmail.com>
2022-03-20 23:48:22 +0545

    Fix: History table migration

--- migration/migration.go
+++ migration/migration.go
@@ -2,10 +2,19 @@ package migration
 
 import (
 	"github.com/NubeIO/flow-framework/migration/versions"
+	"github.com/NubeIO/flow-framework/model"
+	log "github.com/sirupsen/logrus"
 	"gorm.io/gorm"
 )
 
 func AutoMigrate(db *gorm.DB) error {
+	if (db.Migrator().HasColumn(&model.History{}, "idx_histories_id")) {
+		// We drop old table and create new one with composite primary_key
+		// Update of primary_key doesn't support in GORM
+		// Fore more info: https://github.com/go-gorm/gorm/issues/4742
+		err := db.Migrator().DropTable(&model.History{})
+		log.Error(err)
+	}
 	interfaces := versions.GetInitInterfaces()
 	for _, s := range interfaces {
 		if err := db.AutoMigrate(s); err != nil {
--- model/history.go
+++ model/history.go
@@ -4,8 +4,8 @@ import "time"
 
 //History for storing the all history
 type History struct {
-	ID        int       `json:"id" gorm:"AUTO_INCREMENT;primary_key;index"`
-	UUID      string    `json:"uuid"`
-	Value     float64   `json:"value"`
-	Timestamp time.Time `json:"timestamp"`
+	ID        int       `json:"id" gorm:"primary_key"`
+	UUID      string    `json:"uuid" gorm:"primary_key"`
+	Value     float64   `json:"value" gorm:"primary_key"`
+	Timestamp time.Time `json:"timestamp" gorm:"primary_key"`
 }
