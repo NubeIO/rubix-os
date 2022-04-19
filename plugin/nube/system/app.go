9c63f86d3d17b2b32bcbc037f5c04b336c9f041f
Aidan P <aidan@enviroservices.com.au>
2022-04-19 11:55:30 +1000

    made sch work for all schedules now

--- plugin/nube/system/app.go
+++ plugin/nube/system/app.go
@@ -8,7 +8,7 @@ import (
 func (i *Instance) schedule() {
 	j, ok := jobs.GetJobService()
 	if ok {
-		_, err := j.Every(60).Second().Do(i.run)
+		_, err := j.Every(60).Second().Do(i.runSchedule)
 		if err != nil {
 			log.Infof("system-plugin-schedule: error on create job %v\n", err)
 		}
