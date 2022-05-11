40c5cb0d9022c6708b917ab29d0d5af786f5a2e9
NubeDev <aidan@enviroservices.com.au>
2022-05-11 13:43:00 +1000

    remove ping port

--- plugin/nube/protocals/modbusserver/enable.go
+++ plugin/nube/protocals/modbusserver/enable.go
@@ -1,25 +1,14 @@
 package main
 
-import (
-	"fmt"
-	linixpingport "github.com/NubeIO/nubeio-rubix-lib-helpers-go/pkg/networking/linuxpingport"
-)
-
 // Enable implements plugin.Plugin
 func (inst *Instance) Enable() error {
 	inst.enabled = true
 	inst.setUUID()
-
-	p := inst.config.Port
-	if inst.config.Port == 0 {
-		p = 10502
-	}
-
-	port := fmt.Sprintf("%d", p)
-	_, _, foundPort := linixpingport.PingPort("0.0.0.0", port, 1, false)
-	if !foundPort {
-		go inst.serverInit()
-	}
+	//port := fmt.Sprintf("%d", p)
+	//_, _, foundPort := linixpingport.PingPort("0.0.0.0", port, 1, false)
+	//if !foundPort {
+	//	go inst.serverInit()
+	//}
 	return nil
 }
 
