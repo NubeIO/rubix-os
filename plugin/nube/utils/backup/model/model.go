a72e4bd291b7a24603e31393ecb5efc8b1ffade9
RaiBnod <binodrayee@gmail.com>
2022-05-25 16:00:24 +0545

    Upgrade nubeio-rubix-lib-models-go to v1.2.4

--- plugin/nube/utils/backup/model/model.go
+++
@@ -1,8 +0,0 @@
-package model
-
-// Message is a message wrapper with the channel, sender and recipient.
-type Message struct {
-	Msg         plugin.Message
-	ChannelName string
-	IsSend      bool
-}
