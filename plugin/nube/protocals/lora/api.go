d16c1ef69adf384dca0e87d0c80f31fa37713cd8
Shiny380 <daniel.mckinnell43@gmail.com>
2022-10-23 14:56:44 +1100

    bita cleanup

--- plugin/nube/protocals/lora/api.go
+++ plugin/nube/protocals/lora/api.go
@@ -56,12 +56,13 @@ func (inst *Instance) RegisterWebhook(basePath string, mux *gin.RouterGroup) {
 	})
 	mux.PATCH(plugin.NetworksURL, func(ctx *gin.Context) {
 		body, _ := plugin.GetBODYNetwork(ctx)
-		network, err := inst.updateNetwork(body)
+		network, err := inst.db.UpdateNetwork(body.UUID, body, true)
 		api.ResponseHandler(network, err, ctx)
 	})
 	mux.PATCH(plugin.DevicesURL, func(ctx *gin.Context) {
 		body, _ := plugin.GetBODYDevice(ctx)
-		device, err := inst.updateDevice(body)
+		device, err := inst.db.UpdateDevice(body.UUID, body, true)
+		inst.updateDevicePointsAddress(device)
 		api.ResponseHandler(device, err, ctx)
 	})
 	mux.PATCH(plugin.PointsURL, func(ctx *gin.Context) {
@@ -72,22 +73,22 @@ func (inst *Instance) RegisterWebhook(basePath string, mux *gin.RouterGroup) {
 	mux.PATCH(plugin.PointsWriteURL, func(ctx *gin.Context) {
 		body, _ := plugin.GetBodyPointWriter(ctx)
 		uuid := plugin.ResolveID(ctx)
-		point, err := inst.writePoint(uuid, body)
+		point, _, _, _, err := inst.db.PointWrite(uuid, body, false)
 		api.ResponseHandler(point, err, ctx)
 	})
 	mux.DELETE(plugin.NetworksURL, func(ctx *gin.Context) {
 		body, _ := plugin.GetBODYNetwork(ctx)
-		ok, err := inst.deleteNetwork(body)
+		ok, err := inst.db.DeleteNetwork(body.UUID)
 		api.ResponseHandler(ok, err, ctx)
 	})
 	mux.DELETE(plugin.DevicesURL, func(ctx *gin.Context) {
 		body, _ := plugin.GetBODYDevice(ctx)
-		ok, err := inst.deleteDevice(body)
+		ok, err := inst.db.DeleteDevice(body.UUID)
 		api.ResponseHandler(ok, err, ctx)
 	})
 	mux.DELETE(plugin.PointsURL, func(ctx *gin.Context) {
 		body, _ := plugin.GetBODYPoint(ctx)
-		ok, err := inst.deletePoint(body)
+		ok, err := inst.db.DeletePoint(body.UUID)
 		api.ResponseHandler(ok, err, ctx)
 	})
 
