e6d490b88593e9bc0429c545c47d486a66bb5420
Marc Dallen <marc@enviroservices.com.au>
2022-11-16 15:26:57 +1100

    adds network level history enable

--- plugin/nube/protocals/bacnetserver/bserver/model.go
+++ plugin/nube/protocals/bacnetserver/bserver/model.go
@@ -85,6 +85,12 @@ type Network struct {
 		Max      int    `json:"max" default:"200"`
 		Default  string `json:"default" default:""`
 	} `json:"auto_mapping_flow_network_uuid"`
+	AutoMappingEnableHistories struct {
+		Type     string `json:"type" default:"bool"`
+		Required bool   `json:"required" default:"true"`
+		Options  bool   `json:"options" default:"false"`
+		Default  *bool  `json:"default" default:"false"`
+	} `json:"auto_mapping_enable_histories"`
 }
 
 type Device struct {
