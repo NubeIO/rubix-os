9c867fa6af4077bfb1f20ecc6493801dc34e4caa
NubeDev <aidan@enviroservices.com.au>
2023-01-26 12:04:37 +1100

    update lora make address id uppercase

--- src/dbhandler/devices.go
+++ src/dbhandler/devices.go
@@ -3,6 +3,7 @@ package dbhandler
 import (
 	"github.com/NubeIO/flow-framework/api"
 	"github.com/NubeIO/nubeio-rubix-lib-models-go/pkg/v1/model"
+	"strings"
 )
 
 func (h *Handler) GetDevices(args api.Args) ([]*model.Device, error) {
@@ -56,6 +57,7 @@ func (h *Handler) UpdateDeviceErrors(uuid string, body *model.Device) error {
 }
 
 func (h *Handler) UpdateDevice(uuid string, body *model.Device, fromPlugin bool) (*model.Device, error) {
+	body.Model = strings.ToUpper(body.Model)
 	q, err := getDb().UpdateDevice(uuid, body, fromPlugin)
 	if err != nil {
 		return nil, err
