5d5f5b1484ff92bf748e38cf49bfe87b80ac494f
RaiBnod <binodrayee@gmail.com>
2023-06-15 12:59:08 +0545

    Fix: update null views were rejecting

--- database/teamview.go
+++ database/teamview.go
@@ -2,28 +2,30 @@ package database
 
 import (
 	"github.com/NubeIO/nubeio-rubix-lib-models-go/pkg/v1/model"
-	"github.com/NubeIO/rubix-os/utils/nstring"
 	"gorm.io/gorm/clause"
 )
 
 func (d *GormDatabase) UpdateTeamViews(teamUUID string, viewUUIDs []*string) ([]*model.TeamView, error) {
 	views, _ := d.GetViewsByUUIDs(viewUUIDs)
-	var notInUUIDs []*string
+	var notInUUIDs []string
 	var body []*model.TeamView
 	for _, view := range views {
 		var teamViewModel *model.TeamView
 		d.DB.Model(&model.TeamView{}).Where("team_uuid = ?", teamUUID).Where("view_uuid = ?", view.UUID).
 			Find(&teamViewModel)
 		if teamViewModel != nil {
-			notInUUIDs = append(notInUUIDs, nstring.New(view.UUID))
+			notInUUIDs = append(notInUUIDs, view.UUID)
 		}
 		body = append(body, &model.TeamView{
 			TeamUUID: teamUUID,
 			ViewUUID: view.UUID,
 		})
 	}
-	if err := d.DB.Where("team_uuid = ?", teamUUID).Where("view_uuid not in ?", notInUUIDs).
-		Delete(&model.TeamView{}).Error; err != nil {
+	condition := d.DB.Where("team_uuid = ?", teamUUID)
+	if notInUUIDs != nil {
+		condition = condition.Where("view_uuid not in ?", notInUUIDs)
+	}
+	if err := condition.Delete(&model.TeamView{}).Error; err != nil {
 		return nil, err
 	}
 	if len(body) > 0 {
