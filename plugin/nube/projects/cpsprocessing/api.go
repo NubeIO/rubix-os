6d60cc41c57fc8c055d9a18998510d3c5055573f
canuckmarc <marc@enviroservices.com.au>
2023-07-04 07:49:57 +1000

    updates CPS threshold and sites apis

--- plugin/nube/projects/cpsprocessing/api.go
+++ plugin/nube/projects/cpsprocessing/api.go
@@ -10,14 +10,14 @@ func (inst *Instance) RegisterWebhook(basePath string, mux *gin.RouterGroup) {
 
 	sites.POST("", inst.CreateSite)
 	sites.GET("", inst.GetAllSites)
-	sites.GET("/:id", inst.GetSite)
+	sites.GET("/:site_ref", inst.GetSite)
 	sites.POST("/name", inst.GetSiteByName)
 	sites.POST("/address", inst.GetSiteByAddress)
-	sites.PATCH("/:id", inst.UpdateSite)
-	sites.DELETE("/:id", inst.DeleteSite)
+	sites.PATCH("/:site_ref", inst.UpdateSite)
+	sites.DELETE("/:site_ref", inst.DeleteSite)
 
 	thresholds := mux.Group("/thresholds")
 
 	thresholds.POST("", inst.CreateThreshold)
-	thresholds.GET("/:id", inst.GetLastThresholdBySiteRef)
+	thresholds.GET("/:site_ref", inst.GetLastThresholdBySiteRef)
 }
