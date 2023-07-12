f0d9c1bb7d32912f43670960b1738f2535670baf
RaiBnod <binodrayee@gmail.com>
2023-07-12 16:04:00 +0545

    Disable keep-alive

--- runner/runner.go
+++ runner/runner.go
@@ -3,24 +3,22 @@ package runner
 import (
 	"context"
 	"fmt"
+	"github.com/NubeIO/rubix-os/config"
 	log "github.com/sirupsen/logrus"
 	"net"
 	"net/http"
-	"time"
-
-	"github.com/NubeIO/rubix-os/config"
 )
 
 func Run(engine http.Handler, conf *config.Configuration) {
 	addr := fmt.Sprintf("%s:%d", conf.Server.ListenAddr, conf.Server.Port)
 	log.Info("Started Listening for plain HTTP connection on " + addr)
 	server := &http.Server{Addr: addr, Handler: engine}
-	err := server.Serve(startListening(addr, conf.Server.KeepAlivePeriodSeconds))
+	err := server.Serve(startListening(addr))
 	log.Fatal(err)
 }
 
-func startListening(addr string, keepAlive int) net.Listener {
-	lc := net.ListenConfig{KeepAlive: time.Duration(keepAlive) * time.Second}
+func startListening(addr string) net.Listener {
+	lc := net.ListenConfig{KeepAlive: -1}
 	conn, err := lc.Listen(context.Background(), "tcp", addr)
 	if err != nil {
 		log.Fatalln("Could not listen on", addr, err)
